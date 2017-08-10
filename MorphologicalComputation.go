afc2d111bb7e06cb683d9bb569135b41cef65753
Keyan <keyan.zahedi@gmail.com>
2017-08-09 23:35:05 -0600

    Refactoring

--- MorphologicalComputation.go
+++ MorphologicalComputation.go
@@ -8,6 +8,6 @@ func MC_A(pw2a1w1 [][][]float64) float64 {
 	return ConditionalMutualInformation2(pw2a1w1)
 }
 
-func MC_MI(pw2w1 [][]float64, ps1a1 [][]float64) float64 {
-	return MutualInformation2(pw2w1) - MutualInformation2(ps1a1)
+func MC_MI(pw2w1 [][]float64, pa1s1 [][]float64) float64 {
+	return MutualInformation2(pw2w1) - MutualInformation2(pa1s1)
 }
