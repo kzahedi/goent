1fa1a301c6b9b50cec1cee014326b92a95f982ba
Keyan <keyan.zahedi@gmail.com>
2017-08-12 00:27:17 -0600

    Working on Iterative Scaling

--- IterativeScaling.go
+++ IterativeScaling.go
@@ -1,35 +1,43 @@
 package goent
 
 import (
-	"fmt"
 	"math"
+	"sort"
+
+	stat "gonum.org/v1/gonum/stat"
 )
 
 type IterativeScaling struct {
 	P_target              []float64
 	P_estimate            []float64
-	Features              map[string][]int64
-	Nr_of_iterations      int64
+	Features              map[string][]int
+	Nr_of_iterations      int
 	Error_treshold        float64
-	Alphabet              [][]int64
-	Nr_of_states          int64
-	Nr_of_variables       int64
-	Current_feature_index int64
+	Alphabet              [][]int
+	Nr_of_states          int
+	Nr_of_variables       int
+	Current_feature_index int
+	Current_iteration     int
+	Last_KL_step          float64
 	Keys                  []string
 }
 
 // NewIterativeScaling Creates a new struct
 func NewIterativeScaling() *IterativeScaling {
-	return &IterativeScaling{P_target: nil,
-		P_estimate:            nil,
-		Features:              nil,
-		Nr_of_iterations:      0,
-		Error_treshold:        0.0,
+	return &IterativeScaling{
 		Alphabet:              nil,
+		Current_feature_index: -1,
+		Current_iteration:     0,
+		Error_treshold:        0.0,
+		Features:              nil,
+		Last_KL_step:          -1.0,
+		Nr_of_iterations:      0,
 		Nr_of_states:          0,
 		Nr_of_variables:       0,
-		Current_feature_index: -1,
-		Keys: nil}
+		P_estimate:            nil,
+		P_target:              nil,
+		Keys:                  nil,
+	}
 }
 
 // Init extract the feature names for faster processing
@@ -38,21 +46,28 @@ func (data *IterativeScaling) Init() {
 	for k, _ := range data.Features {
 		data.Keys = append(data.Keys, k)
 	}
+	for _, k := range data.Keys {
+		v := data.Features[k]
+		sort.Ints(v)
+		data.Features[k] = v
+	}
+
 	data.Current_feature_index = -1
 	data.P_estimate = make([]float64, len(data.P_target), len(data.P_target))
 	for i, _ := range data.P_target {
 		data.P_estimate[i] = 1.0 / float64(len(data.P_target))
 	}
+	data.CreateAlphabet()
 }
 
 // CreateAlphabet creates the alphabet given Nr_of_states and Nr_of_variables
 func (data *IterativeScaling) CreateAlphabet() {
 
 	n := int(math.Pow(float64(data.Nr_of_states), float64(data.Nr_of_variables)))
-	data.Alphabet = make([][]int64, n, n)
+	data.Alphabet = make([][]int, n, n)
 
 	for i := 0; i < n; i++ {
-		data.Alphabet[i] = make([]int64, data.Nr_of_variables, data.Nr_of_variables)
+		data.Alphabet[i] = make([]int, data.Nr_of_variables, data.Nr_of_variables)
 	}
 
 	nrsi := int(data.Nr_of_states)
@@ -62,7 +77,7 @@ func (data *IterativeScaling) CreateAlphabet() {
 	for i := 0; i < n; i++ {
 		for j := 0; j < nrvi; j++ {
 			b := int(math.Pow(nrsf, float64(j)))
-			w := int64((i / b) % nrsi)
+			w := int((i / b) % nrsi)
 			data.Alphabet[i][nrvi-j-1] = w
 		}
 	}
@@ -73,8 +88,54 @@ func (data *IterativeScaling) CreateAlphabet() {
 // problems. Ann. Probab., 3(1):146–158, 02 1975.
 // Input is a probability distribution, a feature set, and a number of iterations.
 // The output is the maximum entropy estimation of p given the feature set
+// p_est^(n+1)(x) = p_target(x_a) * p_est^(n)(x_{without a}|x_a)
+// where a is cycled through the list of features
+// We calculate it in the following form:
+// p_est^(n+1)(x) =  p_est^(n)(x) * p_target(x_a) / p_est(x_a)
 func (data *IterativeScaling) Iterate() {
 	data.Current_feature_index++
-	feature := data.Features[data.Keys[data.Current_feature_index]]
-	fmt.Println(feature)
+	data.Current_feature_index = int(data.Current_feature_index) % int(len(data.Features))
+	p_copy := make([]float64, len(data.P_estimate), len(data.P_estimate))
+	copy(p_copy, data.P_estimate) // for step with calculation with Kullback-Leibler
+	f := data.Features[data.Keys[data.Current_feature_index]]
+
+	for i, _ := range data.P_estimate {
+		a := calculate_marginal(data.P_target, f, &data.Alphabet, i)
+		b := calculate_marginal(data.P_estimate, f, &data.Alphabet, i)
+		if b > 0 {
+			data.P_estimate[i] = data.P_estimate[i] * a / b
+		}
+	}
+	data.Last_KL_step = stat.KullbackLeibler(p_copy, data.P_estimate)
+}
+
+func calculate_marginal(p []float64, feature []int, alphabet *[][]int, index int) float64 {
+	sum := 0.0
+	indices := Get_alphabet_indices(index, feature, alphabet)
+	if len(indices) > 0 {
+		for _, v := range indices {
+			sum += p[v]
+		}
+	}
+	return sum
+}
+
+func Get_alphabet_indices(index int, feature []int, alphabet *[][]int) []int {
+	a := (*alphabet)[index]
+	var indices []int
+	for i, v := range *alphabet {
+		if Check_feature_alphabet(feature, a, v) == true {
+			indices = append(indices, i)
+		}
+	}
+	return indices
+}
+
+func Check_feature_alphabet(feature, values, alphabet []int) bool {
+	for _, v := range feature {
+		if values[v] != alphabet[v] {
+			return false
+		}
+	}
+	return true
 }
--- IterativeScaling_test.go
+++ IterativeScaling_test.go
@@ -1,15 +1,16 @@
 package goent_test
 
 import (
-	"fmt"
+	"math"
 	"testing"
 
 	"github.com/kzahedi/goent"
+	stat "gonum.org/v1/gonum/stat"
 )
 
-// alphabet         [][]int64
+// alphabet         [][]int
 
-func TestIterativeScalingIAND(t *testing.T) {
+func TestIterativeScalingAND(t *testing.T) {
 	data := goent.IterativeScaling{}
 
 	data.Nr_of_variables = 3
@@ -20,22 +21,105 @@ func TestIterativeScalingIAND(t *testing.T) {
 	data.P_target[4] = 0.25
 	data.P_target[6] = 0.25
 
-	data.Features = make(map[string][]int64)
-	data.Features["X,Z"] = []int64{0, 2}
-	data.Features["Y,Z"] = []int64{1, 2}
-	data.Features["X,Y"] = []int64{0, 1}
+	data.Features = make(map[string][]int)
+	data.Features["X,Z"] = []int{0, 2}
+	data.Features["Y,Z"] = []int{1, 2}
+	data.Features["X,Y"] = []int{0, 1}
 
 	data.Nr_of_iterations = 10
 	data.Error_treshold = 0.0
 
 	data.Init()
-	data.CreateAlphabet()
-	data.Iterate()
+	for i := 0; i < 100; i++ {
+		data.Iterate()
+	}
 
-	// 0 0 0
-	// 0 1 0
-	// 1 0 0
-	// 1 1 1
+	r := stat.KullbackLeibler(data.P_target, data.P_estimate)
 
-	fmt.Println(data)
+	if r > 0.0001 {
+		t.Errorf("AND should be 0 but it is ", r)
+	}
+}
+
+func TestIterativeScalingXOR(t *testing.T) {
+	data := goent.IterativeScaling{}
+
+	data.Nr_of_variables = 3
+	data.Nr_of_states = 2
+
+	data.P_target = make([]float64, 8, 8)
+	data.P_target[0] = 0.25
+	data.P_target[3] = 0.25
+	data.P_target[5] = 0.25
+	data.P_target[7] = 0.25
+
+	data.Features = make(map[string][]int)
+	data.Features["X,Z"] = []int{0, 2}
+	data.Features["Y,Z"] = []int{1, 2}
+	// data.Features["X,Y"] = []int{0, 1}
+
+	data.Nr_of_iterations = 10
+	data.Error_treshold = 0.0
+
+	data.Init()
+	for i := 0; i < 1000; i++ {
+		data.Iterate()
+	}
+
+	r := stat.KullbackLeibler(data.P_target, data.P_estimate)
+
+	if math.Abs(r-1.0) > 0.00001 {
+		t.Errorf("XOR should be 1 but it is ", r)
+	}
+}
+
+func TestCheckFeature(t *testing.T) {
+	alph := []int{10, 20, 30, 40, 50}
+	a := []int{10, 20, 30, 40, 50}
+	f := []int{1, 3}
+
+	if goent.Check_feature_alphabet(f, a, alph) == false {
+		t.Errorf("Check_feature_alphabet(", f, ",", a, ",", alph, ") should be true")
+	}
+
+	a = []int{100, 200, 300, 400, 500}
+	g := []int{1, 2}
+	if goent.Check_feature_alphabet(g, a, alph) == true {
+		t.Errorf("Check_feature_alphabet(", g, ",", a, ",", alph, ") should be false")
+	}
+}
+
+func TestGetAlphabetIndices(t *testing.T) {
+	alphabet := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{1, 9, 3},
+		{7, 8, 9},
+		{1, 0, 3},
+		{1, 5, 3}}
+	f := []int{0, 2}
+	index := 0
+
+	indices := goent.Get_alphabet_indices(index, f, &alphabet)
+
+	if len(indices) != 4 {
+		t.Errorf("Get_alphabet_indices should return 4 values, but only has ", len(indices))
+	}
+
+	if indices[0] != 0 {
+		t.Errorf("First index should be 0")
+	}
+
+	if indices[1] != 2 {
+		t.Errorf("Second index should be 2")
+	}
+
+	if indices[2] != 4 {
+		t.Errorf("Third index should be 4")
+	}
+
+	if indices[3] != 5 {
+		t.Errorf("Fourth index should be 5")
+	}
+
 }
