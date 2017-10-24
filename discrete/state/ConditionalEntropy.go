59607e6266fce80cfe83caecee5aeebe3964455d
Keyan <keyan.zahedi@gmail.com>
2017-10-23 22:43:25 -0600

    H_t(X|Y)

---
+++ discrete/state/ConditionalEntropy.go
@@ -0,0 +1,50 @@
+package state
+
+import (
+	"math"
+
+	"github.com/kzahedi/goent/discrete"
+)
+
+// ConditionalEntropy calculates the conditional entropy of a probability distribution.
+// It takes the log function as an additional parameter, so that the base
+// can be chosen
+//   H(X|Y) = -\sum_x p(x,y) lnFunc(p(x,y)/p(y))
+func ConditionalEntropy(data [][]int, log lnFunc) []float64 {
+	pxy := discrete.Emperical2D(data)
+	xDim := len(pxy)
+	yDim := len(pxy[0])
+
+	r := make([]float64, len(data), len(data))
+	py := make([]float64, yDim, yDim)
+
+	for y := 0; y < yDim; y++ {
+		py[y] = 0.0
+		for x := 0; x < xDim; x++ {
+			py[y] += pxy[x][y]
+		}
+	}
+
+	for i := range data {
+		x := data[i][0]
+		y := data[i][1]
+		if py[y] > 0.0 && pxy[x][y] > 0.0 {
+			r[i] = -(log(pxy[x][y]) - log(py[y]))
+		}
+	}
+	return r
+}
+
+// ConditionalEntropyBaseE calculates the conditional entropy of a
+// probability distribution in nats
+//   H(X|Y) = -\sum_x p(x,y) ln(p(x,y)/p(y))
+func ConditionalEntropyBaseE(data [][]int) []float64 {
+	return ConditionalEntropy(data, math.Log)
+}
+
+// ConditionalEntropyBase2 calculates the conditional entropy of a
+// probability distribution in bits
+//   H(X|Y) = -\sum_x p(x,y) log2(p(x,y)/p(y))
+func ConditionalEntropyBase2(data [][]int) []float64 {
+	return ConditionalEntropy(data, math.Log2)
+}
---
+++ discrete/state/ConditionalEntropy_test.go
@@ -0,0 +1,130 @@
+package state_test
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/kzahedi/goent/discrete"
+	"github.com/kzahedi/goent/discrete/state"
+)
+
+func TestConditionalEntropyBase2(t *testing.T) {
+	t.Log("Testing Conditional Entropy")
+	p1 := [][]float64{
+		{1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0},
+		{1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0},
+		{1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0},
+		{1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0}}
+
+	d1 := [][]int{
+		{0, 0}, {0, 1}, {0, 2}, {0, 3},
+		{1, 0}, {1, 1}, {1, 2}, {1, 3},
+		{2, 0}, {2, 1}, {2, 2}, {2, 3},
+		{3, 0}, {3, 1}, {3, 2}, {3, 3}}
+
+	avg := discrete.ConditionalEntropyBase2(p1)
+	s := state.ConditionalEntropyBase2(d1)
+	fmt.Println(s)
+	r := 0.0
+	for _, v := range s {
+		r += v
+	}
+
+	r /= float64(len(s))
+
+	diff := avg - r
+
+	if diff > 0.00001 {
+		t.Errorf("Discrete (%f) vs. state (%f) must be equal the difference is %f", avg, r, diff)
+	}
+}
+
+func TestConditionalEntropyBase2Zero(t *testing.T) {
+	t.Log("Testing Conditional Entropy")
+
+	p1 := [][]float64{
+		{1.0 / 4.0, 0.0, 0.0, 0.0},
+		{0.0, 1.0 / 4.0, 0.0, 0.0},
+		{0.0, 0.0, 1.0 / 4.0, 0.0},
+		{0.0, 0.0, 0.0, 1.0 / 4.0}}
+
+	d1 := [][]int{
+		{0, 0}, {0, 0}, {0, 0}, {0, 0},
+		{1, 1}, {1, 1}, {1, 1}, {1, 1},
+		{2, 2}, {2, 2}, {2, 2}, {2, 2},
+		{3, 3}, {3, 3}, {3, 3}, {3, 3}}
+
+	avg := discrete.ConditionalEntropyBase2(p1)
+	s := state.ConditionalEntropyBase2(d1)
+	r := 0.0
+	for _, v := range s {
+		r += v
+	}
+	r /= float64(len(s))
+
+	diff := avg - r
+
+	if math.Abs(avg-r) > 0.00001 {
+		t.Errorf("Discrete (%f) vs. state (%f) must be equal the difference is %f", avg, r, diff)
+	}
+}
+
+func TestConditionalEntropyBaseE(t *testing.T) {
+	t.Log("Testing Conditional Entropy")
+	p1 := [][]float64{
+		{1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0},
+		{1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0},
+		{1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0},
+		{1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0, 1.0 / 16.0}}
+
+	d1 := [][]int{
+		{0, 0}, {0, 1}, {0, 2}, {0, 3},
+		{1, 0}, {1, 1}, {1, 2}, {1, 3},
+		{2, 0}, {2, 1}, {2, 2}, {2, 3},
+		{3, 0}, {3, 1}, {3, 2}, {3, 3}}
+
+	avg := discrete.ConditionalEntropyBaseE(p1)
+	s := state.ConditionalEntropyBaseE(d1)
+	r := 0.0
+	for _, v := range s {
+		r += v
+	}
+	r /= float64(len(s))
+
+	diff := avg - r
+
+	if math.Abs(avg-r) > 0.00001 {
+		t.Errorf("Discrete (%f) vs. state (%f) must be equal the difference is %f", avg, r, diff)
+	}
+}
+
+func TestConditionalEntropyBaseEZero(t *testing.T) {
+	t.Log("Testing Conditional Entropy")
+
+	p1 := [][]float64{
+		{1.0 / 4.0, 0.0, 0.0, 0.0},
+		{0.0, 1.0 / 4.0, 0.0, 0.0},
+		{0.0, 0.0, 1.0 / 4.0, 0.0},
+		{0.0, 0.0, 0.0, 1.0 / 4.0}}
+
+	d1 := [][]int{
+		{0, 0}, {0, 0}, {0, 0}, {0, 0},
+		{1, 1}, {1, 1}, {1, 1}, {1, 1},
+		{2, 2}, {2, 2}, {2, 2}, {2, 2},
+		{3, 3}, {3, 3}, {3, 3}, {3, 3}}
+
+	avg := discrete.ConditionalEntropyBaseE(p1)
+	s := state.ConditionalEntropyBaseE(d1)
+	r := 0.0
+	for _, v := range s {
+		r += v
+	}
+	r /= float64(len(s))
+
+	diff := avg - r
+
+	if math.Abs(avg-r) > 0.00001 {
+		t.Errorf("Discrete (%f) vs. state (%f) must be equal the difference is %f", avg, r, diff)
+	}
+}
