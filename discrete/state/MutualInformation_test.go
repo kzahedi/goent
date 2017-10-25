741209c46d26b1546a8b19b85300cde46ccc26ba
Keyan <keyan.zahedi@gmail.com>
2017-10-25 00:19:17 -0600

    Fixed test cases

--- discrete/state/MutualInformation_test.go
+++ discrete/state/MutualInformation_test.go
@@ -35,3 +35,30 @@ func TestMutualInformation(t *testing.T) {
 		}
 	}
 }
+
+func TestMutualInformationBaseE(t *testing.T) {
+	t.Log("Testing Mutual Information")
+	data := make([][]int, 100)
+	for j := 0; j < 100; j++ {
+		for i := 0; i < 100; i++ {
+			data[i] = make([]int, 2, 2)
+			data[i][0] = int(rand.Int63n(100))
+			data[i][1] = int(rand.Int63n(100))
+		}
+
+		r := state.MutualInformationBaseE(data)
+		p := discrete.Emperical2D(data)
+		mi := discrete.MutualInformationBaseE(p)
+
+		s := 0.0
+		for i := 0; i < len(r); i++ {
+			s += r[i]
+		}
+
+		s /= float64(len(r))
+
+		if math.Abs(mi-s) > 0.00001 {
+			t.Errorf("Mutual information (Base E) should be equal %f = %f", s, mi)
+		}
+	}
+}
